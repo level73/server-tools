b4e2f06f7fa312b84ecc367e9142481038858db0
mrsparkl3 <ste.gambardella@gmail.com>
2020-10-09 10:22:48 +0200

    checking existing files debugged

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 #makes sure user is root
-if ["$(whoami)"!= 'root']; then
+if [ "$(whoami)" != 'root' ]; then
     echo "Please run the installation script with sudo privileges..."
     exit 1
 fi
 
 #Adds executable permission to addhost file, if exists, and copies it to /usr/local/bin
 
-if [ -f "/addhost.sh"]; then
+if [ -e "addhost.sh" ]; then
     chmod +x addhost.sh
 
     #checks if there is an older version of addhost file in /usr/local/bin 
-    if [ -f "/usr/local/bin/addhost"]; then
+    if [ -e "/usr/local/bin/addhost" ]; then
     echo "addhost file already exists..."
     echo "Do you want to remove it and install a new version?"
     select yn in "Yes" "No"; do
@@ -27,7 +27,7 @@ else
     echo "There is no addhost.sh file in your working directory, please add it"
     exit 1
 fi
-if [-f "/usr/local/bin/addhost"]; then
+if [ -e "/usr/local/bin/addhost" ]; then
     echo "Installation completed."
 else
     echo "Failed Installing..."
