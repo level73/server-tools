3ac778e1d736a7a5055974cd23254a6613b3be38
mrsparkl3 <ste.gambardella@gmail.com>
2020-10-12 12:56:28 +0200

    exper. with querys, still not working

--- setmysql.sh
+++ setmysql.sh
@@ -6,6 +6,10 @@
 #4. Give privileges to new user for the new database
 #5. Versarmi una birra
 
+# revisiting architecture 12/10/2020
+# create a db, asks for what user you want to add permission, if user exists >> add permission to db
+# if user do not exists, create a new user and add permission to db
+
 #makes sure user is root
 if [ "$(whoami)" != 'root' ]; then
     echo "Please run the script with sudo privileges..."
@@ -13,26 +17,43 @@ if [ "$(whoami)" != 'root' ]; then
 fi
 
 #checking if mysql is installed, if not >> exit
-type mysql >/dev/null 2>&1 && echo "MySQL present." || echo "MySQL not present." exit 1
+type mysql >/dev/null 2>&1 && echo "MySQL present." || echo "MySQL not present." || exit 1
 
-read -p "Type a name for the new user:" username
-read -p "Type a new password for ${username}:" password
-read -p "Type a new database name:" dbname
+#read -p "Type a new database name:" dbname
+read -p "Type what USER to grant permission for \" ${dbname} \":" username
+#read -p "Type a new password for ${username}:" password
+
 #echo "${username} ${password} ${dbname}"
 
 
 #1. access to MySQL as root
 # Could be to run this line at the end with <<EOF
-mysql -u root -p || exit 1
+echo "Connecting to MYSQL as root..."
+mysql -u root -p << EOF
+\! echo "connected to MYSQL!!!"
 
-#2. creates new database 
-CREATE DATABASE ${dbname};
+#creates new database  (works)
+#\! echo "Creating ${dbname} database..."   
+#CREATE DATABASE IF NOT EXISTS ${dbname};
 
-#create new user
-CREATE USER \'${username}\'@\'localhost\' IDENTIFIED BY \'${password}\';
+#checks for username, if not exists, creates it
+IF (SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '${username}')) = 1 THEN
+    \! echo "Username exists."
 
-#4. gives the new user all privileges for new db
-GRANT ALL ON ${dbname}.* TO \'${username}\'@\'localhost\';
-flush privileges;
+ELSE
+    \! echo "Username \" ${username} \" NOT exists. Creating it for you."
+    \! read -p "Type a password for ${username}:" password
+    CREATE USER IF NOT EXISTS '${username}'@'localhost' IDENTIFIED BY '${password}';
+        
+END IF
+
+#gives the user all privileges for new db
+#GRANT ALL ON ${dbname}.* TO '${username}'@'localhost';
+#flush privileges;
 
 
+use mysql;
+select user from user;
+#SHOW DATABASES;
+
+EOF
