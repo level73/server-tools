e7f97e81438dae39d792a90f2f5b5839fb5290fe
mrsparkl3 <ste.gambardella@gmail.com>
2022-09-20 12:21:29 +0200

    added prod or dev option (to be tested)

--- setssl.sh
+++ setssl.sh
@@ -6,7 +6,16 @@ if [ "$(whoami)" != "root" ]; then
 	exit 2
 fi
 
+#!!! make sure to pass the site url as argument, WITHOUT "www." !!!
+
 #command for activate ssl, using global variable "site_url"
 echo "setting SSL certificate with certbot..."
 
-(systemctl `certbot --apache -d $1` && echo "Certbot setting completed.") || echo "Certbot setting FAILED..." 
+read -p "Is this a Production site? (NO if DEVELOPMENT site) [Y/n]" yn
+case $yn in
+	[Yy]* ) echo "Setting certbot for BOTH www and plane url..." && 
+	(certbot --apache -d $1 -d www.$1 && echo "Certbot setting completed.") || echo "Certbot setting FAILED..." ;;
+	[Nn]* ) echo "Setting certbot for plane url ONLY..." && 
+	(certbot --apache -d $1 && echo "Certbot setting completed.") || echo "Certbot setting FAILED...";;
+	* ) echo "Please answer yes or no.";;      
+	esac
